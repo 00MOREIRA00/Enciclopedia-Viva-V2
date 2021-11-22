59549a95861e6bfc5264e0dc8174eb83ad8bcc55
00MOREIRA00 <85044936+00MOREIRA00@users.noreply.github.com>
2021-11-22 14:35:03 -0300

    Add files via upload

---
+++ conteudos/historia/pre_historia_funcoes.js
@@ -0,0 +1,27 @@
+/*INICIO CONFIG TRANFERENCIA AULA*/
+function verificaTecla4(){
+		    document.addEventListener('keydown', prehistoriaKD);
+		}
+
+
+function prehistoriaKD(){
+		//A = 65; S = 83; D = 68; F = 70; G = 71; H = 72; Z = 90;
+			var tecla4 = window.event.keyCode;
+			if(tecla4 == 13){
+				  responsiveVoice.speak('Para iniciar audio conteúdo clique seta para cima, para pausar audio conteúdo clique seta para paixo ,para iniciar o quiz tecle espaço e para voltar ao menu tecle control', 'Brazilian Portuguese Female');
+			}else if(tecla4 == 38){
+				const audio = document.querySelector('audio')
+					audio.play()			
+			}else if (tecla4 == 40){
+				const audio = document.querySelector('audio')
+					audio.pause()	
+			}else if (tecla4 == 32){
+				responsiveVoice.speak('Você Inicio o Quiz tecle Enter para ouvir a primeira pergunta', 'Brazilian Portuguese Female');
+				 window.location.href = 'perguntas/pergunta9.html'
+			}else if (tecla4 == 17) {
+				responsiveVoice.speak('Você voltou a tela anterior, tecle seta para baixo para saber os conteúdos', 'Brazilian Portuguese Female');
+				 window.location.href = 'menu_historia.html'
+			}
+	}
+			verificaTecla4()
+/*FIM CONFIG TRANFERENCIA AULA*/
---
+++ conteudos/matematica/menu_matematica_funcoes.js
@@ -0,0 +1,27 @@
+/*INICIO CONFIG TRANFERENCIA AULA*/
+function verificaTecla3(){
+		    document.addEventListener('keydown', historiaKD);
+		}
+
+
+function historiaKD(){
+		//A = 65; S = 83; D = 68; F = 70; G = 71; H = 72; Z = 90;
+			var tecla3 = window.event.keyCode;
+			if(tecla3 == 40){
+				  responsiveVoice.speak('Clique A para acessar a aula sobre Soma, clique S para acessar a aula sobre o Subtração, clique control para voltar ao menu principal', 'Brazilian Portuguese Female');
+			}else if(tecla3 == 65){
+				responsiveVoice.speak("Você acessou a aula de Meio Ambiente, para saber mais das nossas configurações clique enter", 'Brazilian Portuguese Female');
+				window.location.href = 'meio_ambiente.html';				
+		} else if(tecla3 == 17){
+				responsiveVoice.speak("Você voltou a tela inicial, para saber dos conteúdos tecle seta para baixo", 'Brazilian Portuguese Female');
+				window.location.href = '../../index.html'
+		}else if(tecla3 == 83){
+				responsiveVoice.speak("Você acessou a aula de Povoamento Brasileiro, para saber mais das nossas configurações clique enter", 'Brazilian Portuguese Female');
+				window.location.href = 'povoamento_brasileiro.html';
+		}else if(tecla3 == 68){
+				responsiveVoice.speak("Você acessou a aula de Pré-História, para saber mais das nossas configurações clique enter", 'Brazilian Portuguese Female');
+				window.location.href = 'pre_historia.html';
+		}
+	}
+			verificaTecla3()
+/*FIM CONFIG TRANFERENCIA AULA*/
